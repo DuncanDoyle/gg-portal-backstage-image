be837b0925ef04619bd6b96826fabd675ace1ce9
Duncan Doyle <Duncan.Doyle@gmail.com>
2023-07-20 22:01:48 +0200

    No longer using caching in Docker build.

--- build-backstage-image-with-gloo-platform-portal.sh
+++ build-backstage-image-with-gloo-platform-portal.sh
@@ -139,6 +139,6 @@ echo "Generate the Docker image.\n"
 #yarn build-image --tag backstage:$DOCKER_IMAGE_TAG
 # Not using yarn build-image, as we want to do a multi-arch build.
 
-docker buildx build -f packages/backend/Dockerfile --platform linux/amd64,linux/arm64 --push -t duncandoyle/backstage-gloo -t duncandoyle/backstage-gloo:$DOCKER_IMAGE_TAG .
+docker buildx build -f packages/backend/Dockerfile --platform linux/amd64,linux/arm64 --no-cache --push -t duncandoyle/backstage-gloo -t duncandoyle/backstage-gloo:$DOCKER_IMAGE_TAG .
 
 echo "Backstage Docker image created successfully with name 'backstage-gloo' and tag '$DOCKER_IMAGE_TAG'.\n"
